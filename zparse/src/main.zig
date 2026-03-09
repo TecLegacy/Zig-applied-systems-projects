6ad963d62d20c1076fca83b4060dd6ee1d37f6c7
tectron <kshvkumar.kk2@gmail.com>
2026-03-09 19:28:06 +0530

    zig fundamentals and memory
    improved flags capture

--- zparse/src/main.zig
+++ zparse/src/main.zig
@@ -1,74 +1,92 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
-    const val = parseArgs(init) catch |err| {
-        std.debug.print("Error: While parsing args {} \n", .{err});
+    const flag = parseArgs(init) catch |err| {
+        switch (err) {
+            error.NoFlagsPassed, error.UnknownFlag => return,
+        }
         return;
     };
-    std.debug.print("Val: {} \n", .{val});
+
+    if (flag.show_help) {
+        flagUsage();
+        return;
+    }
+
+    std.debug.print(
+        "Flags passed:\n  hello={} help={} info={} warn={} error={}\n",
+        .{ flag.show_hello, flag.show_help, flag.show_info, flag.show_warn, flag.show_error },
+    );
 }
+
+// Track which flag was sent
+const Flags = struct {
+    show_hello: bool = false,
+    show_help: bool = false,
+    show_info: bool = false,
+    show_warn: bool = false,
+    show_error: bool = false,
+};
 
 // Reading File from CLI and printing usage
 // zparse --info log-file
-fn parseArgs(init: std.process.Init) !bool {
+fn parseArgs(init: std.process.Init) !Flags {
     var args = try init.minimal.args.iterateAllocator(init.gpa);
     defer args.deinit();
 
     // skip program name
     _ = args.skip();
 
-    // Command and log level flags
-    var show_hello = false;
-    var show_help = false;
-    var show_info = false;
-    var show_warn = false;
-    var show_error = false;
+    var flag = Flags{};
+
+    // Making sure user passes a flag to work with
+    var is_flag_passed = false;
 
     // accepting flags --info --warn --error (-i,-w,-e)
     while (args.next()) |arg| {
         if (std.mem.eql(u8, arg, "--hello")) {
-            show_hello = true;
+            is_flag_passed = true;
+            flag.show_hello = true;
             continue;
         }
 
         if (std.mem.eql(u8, arg, "--help") or std.mem.eql(u8, arg, "-h")) {
-            show_help = true;
+            is_flag_passed = true;
+            flag.show_help = true;
             continue;
         }
 
         if (std.mem.eql(u8, arg, "--info") or std.mem.eql(u8, arg, "-i")) {
-            show_info = true;
+            is_flag_passed = true;
+            flag.show_info = true;
             continue;
         }
 
         if (std.mem.eql(u8, arg, "--warn") or std.mem.eql(u8, arg, "-w")) {
-            show_warn = true;
+            is_flag_passed = true;
+            flag.show_warn = true;
             continue;
         }
 
         if (std.mem.eql(u8, arg, "--error") or std.mem.eql(u8, arg, "-e")) {
-            show_error = true;
+            is_flag_passed = true;
+            flag.show_error = true;
             continue;
         }
 
         if (std.mem.startsWith(u8, arg, "-")) {
             std.debug.print("Unknown flag: {s}\n", .{arg});
             flagUsage();
-            return false;
+            return error.UnknownFlag;
         }
     }
 
-    if (show_help) {
+    if (!is_flag_passed) {
         flagUsage();
-        return true;
+        return error.NoFlagsPassed;
     }
 
-    std.debug.print(
-        "Flags passed:\n  hello={} help={} info={} warn={} error={}\n",
-        .{ show_hello, show_help, show_info, show_warn, show_error },
-    );
-
-    return show_hello or show_help or show_info or show_warn or show_error;
+    return flag;
 }
 
 // Print flag usage
@@ -87,3 +105,12 @@ fn flagUsage() void {
         \\
     , .{});
 }
+
+// File handling
+// fn fileHandler(io: std.Io, cwd: std.Io.Dir.cwd()) void {
+//     // const file_os_handle = cwd.openFile(io, file_path, {});
+
+//     // open close file , handle error
+//     // chunk read
+//     // check for warning level
+// }
